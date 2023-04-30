756a61291dc46c5b761e75a69f2bfdcfe406b365
peasantdev <peasantwebdev@outlook.com>
2023-04-29 21:22:25 -0700

    Add project files.

---
+++ ConsoleApp6/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee(); //Creating the employee object
+            myEmployee.employeestate();
+        }
+
+    }
+}
---
+++ ConsoleApp6/Interface1.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    interface IQuittable// Interface.
+    {
+        void employeestate();
+    }
+}
+
+class Employee : IQuittable //Polymorphism to create the Iquittable object.
+{
+    public void employeestate()//Calling the method.
+    {
+        Console.WriteLine("The Employee has left");//Output of the method.
+        Console.ReadLine();
+    }
+}
+       
